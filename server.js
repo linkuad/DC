ab77ca3137bc91ec92a3d15daf83858e4af95e50
Linkuad <linkuad@gmail.com>
2020-12-16 14:10:18 +0000

    Feat: add cookie-parser

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const concat = require("concat-stream");
-const { user } = require("./models/user.js");
+const { User } = require("./models/User.js");
 const mongoose = require("mongoose");
 const path = require("path");
 const port = 3000;
+const cookieParser = require('cookie-parser');
 
 const config = require('./config/key.js')// api키 라던지 깃허브에 노출 되면 안되는 값들
 
@@ -12,19 +13,34 @@ const config = require('./config/key.js')// api키 라던지 깃허브에 노출
 
 //app.use('/api/users', require('./routes/users'));
 app.use(express.static("client"));
+app.use(express.json());//기존 bodyparser 의 기능
 
-// mongoose.connect(config.mongoURI, {//connect to MongoDB
-// useUnifiedTopology : true,
-// useNewUrlParser : true,
-// })
-// .then (() => console.log ( 'DB Connected!'))
-// .catch (err => {
-// console.log (`DB Connection Error: ${err.message}`);
-// });
+
+mongoose.connect(config.mongoURI, {//connect to MongoDB
+	useUnifiedTopology : true,
+	useNewUrlParser : true,
+})
+.then (() => console.log ( 'DB Connected!'))
+.catch (err => {
+	console.log (`DB Connection Error: ${err.message}`);
+});
 
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname + "/client/index.html"));
 });
+
+app.post('/users/register',(req,res) => {// register function
+	const user = new User(req.body)
+	user.save((err,doc) => {
+		
+		if(err) res.json({success:false,err})
+		return res.status(200).json({success:true,doc})//save실패하면 에러를 퉤 성공하면 정보를 퉤
+	})
+})
+
+app.post('/users/login',(req,res)=>{// login function 
+	
+})
 
 app.listen(port, _ => {
 	console.log("express server open, port : " + port);
