120ea6b0170ecfa600d689375430d788d73e51d7
wonsunil <seonil6352@gmail.com>
2020-12-17 00:30:32 +0900

    Refactor: simply code

--- client/js/login.js
+++ client/js/login.js
@@ -23,30 +23,51 @@ const focus =ele=>{
     ele.parentNode.style.border = '1px solid #74C69D';
 }
 
-const inputChk =ele=>{
-    let err = ele.parentNode.children[2];
-    err.innerHTML = '';
-    if(ele == inputs.email){
-        var reg = /^[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[@]{1}[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[.]{1}[A-Za-z]{1,5}$/;
-        if(!reg.test(ele.value)){
-            err.innerHTML = '이메일을 잘못 입력했습니다.'
-        }
-    }else if(ele == inputs.password){
-        var reg = /(?=.*[a-zA-Z]+)(?=.*[0-9]+)(?=.*[`~!@@#$%^&*|₩₩₩'₩";:₩/?]+).{8,20}/;
-        if(!reg.test(ele.value)){
-            err.innerHTML = '비밀번호는 대문자, 특수문자를 포함하여 8자 이상 입력해야 합니다.';
-        }
-    }
-    ele.parentNode.style.border = '1px solid #ddd';
-}
+const inputValidateCheck = target => {
+    const $err = target.parentNode.children[2];
+    $err.innerHTML = "";
 
-window.onload = () =>{
-    inputs.email.addEventListener('input', () => showErase(inputs.email));
-    inputs.password.addEventListener('input', () => showErase(inputs.password));
+    if(target.name === "email") {
+        const reg = /^[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[@]{1}[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[.]{1}[A-Za-z]{1,5}$/;
+        
+        if (!reg.test(target.value)) {
+            target.focus();
+                
+            return $err.innerHTML = "이메일을 잘못 입력했습니다.";
+        };
+    };
+    if(target.name === "password") {
+        const reg = /(?=.*[a-zA-Z]+)(?=.*[0-9]+).{8,20}/;
+                
+        if (!reg.test(target.value)) {
+            target.focus();
+                    
+            return $err.innerHTML = "비밀번호는 대문자 포함하여 8자 이상 입력해야 합니다.";
+        };
+    };
 
-    inputs.email.addEventListener('focus', () => focus(inputs.email));
-    inputs.password.addEventListener('focus', () => focus(inputs.password));
-	
-    inputs.email.addEventListener('focusout', () => inputChk(inputs.email));
-    inputs.password.addEventListener('focusout', () => inputChk(inputs.password));
-}
+    return true;
+};
+
+window.onload = () => {
+    const $inputs = [...document.querySelectorAll("form > div > input")];
+
+    document.forms[0].addEventListener("submit", event => {
+        event.preventDefault();
+        
+        if(inputValidateCheck) event.target.submit();
+    });
+
+    $inputs.forEach(item => {
+        item.addEventListener("focus", ({ target }) => {
+            target.parentNode.style.border = "1px solid black";
+        });
+    });
+    $inputs.forEach(item => {
+        item.addEventListener("focusout", ({ target }) => {
+            inputValidateCheck(target);
+            
+            target.parentNode.style.border = "#ddd";
+        });
+    });
+};
