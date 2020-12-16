b5f4932f96f0971d82a78f6742f9a6493d421895
Linkuad <linkuad@gmail.com>
2020-12-16 16:00:25 +0000

    Refactor: merge git tree

--- client/js/login.js
+++ client/js/login.js
@@ -1,52 +1,48 @@
-const inputs = {
-    email : document.querySelector('input[name=email]'),
-    password : document.querySelector('input[name=password]'),
-}
+const inputValidateCheck = target => {
+    const $err = target.parentNode.children[2];
+    $err.innerHTML = "";
 
-const erase = ele =>{
-    ele.parentNode.children[0].value = '';
-    ele.parentNode.children[1].style.display = 'none';
-    ele.parentNode.children[0].focus();
-    ele.parentNode.children[2].innerHTML = '';
-}
+    if(target.name === "email") {
+        const reg = /^[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[@]{1}[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[.]{1}[A-Za-z]{1,5}$/;
+        
+        if (!reg.test(target.value)) {
+            target.focus();
+                
+            return $err.innerHTML = "이메일을 잘못 입력했습니다.";
+        };
+    };
+    if(target.name === "password") {
+        const reg = /(?=.*[a-zA-Z]+)(?=.*[0-9]+).{8,20}/;
+                
+        if (!reg.test(target.value)) {
+            target.focus();
+                    
+            return $err.innerHTML = "비밀번호는 대문자 포함하여 8자 이상 입력해야 합니다.";
+        };
+    };
 
-const showErase =ele=> {
-    ele.parentNode.children[2].innerHTML = '';
-    if(ele.value.length > 0){
-        ele.parentNode.children[1].style.display = 'block';
-    }else{
-        ele.parentNode.children[1].style.display = 'none';
-    }
-}
+    return true;
+};
 
-const focus =ele=>{
-    ele.parentNode.style.border = '1px solid #74C69D';
-}
+window.onload = () => {
+    const $inputs = [...document.querySelectorAll("form > div > input")];
 
-const inputChk =ele=>{
-    let err = ele.parentNode.children[2];
-    err.innerHTML = '';
-    if(ele == inputs.email){
-        var reg = /^[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[@]{1}[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[.]{1}[A-Za-z]{1,5}$/;;  
-        if(!reg.test(ele.value)){
-            err.innerHTML = '이메일을 잘못 입력했습니다.'
-        }
-    }else if(ele == inputs.password){
-        //var reg = /(?=.*[a-zA-Z]+)(?=.*[0-9]+)(?=.*[`~!@@#$%^&*|₩₩₩'₩";:₩/?]+).{8,20}
-		var reg = /(?=.*[a-zA-Z]+)(?=.*[0-9]+).{8,20}/;
-        if(!reg.test(ele.value)){
-            err.innerHTML = '비밀번호는 대문자를 포함하여 8자 이상 입력해야 합니다.'
-        }
-    }
-    ele.parentNode.style.border = '1px solid #ddd';
-}
-window.onload =_=>{
-    inputs.email.addEventListener('input', () => showErase(inputs.email))
-    inputs.password.addEventListener('input', () => showErase(inputs.password))
-    
-    inputs.email.addEventListener('focusout', () => inputChk(inputs.email))
-    inputs.password.addEventListener('focusout', () => inputChk(inputs.password))
+    document.forms[0].addEventListener("submit", event => {
+        event.preventDefault();
+        
+        if(inputValidateCheck) event.target.submit();
+    });
 
-    inputs.email.addEventListener('focus', () => focus(inputs.email))
-    inputs.password.addEventListener('focus', () => focus(inputs.password))
-}
+    $inputs.forEach(item => {
+        item.addEventListener("focus", ({ target }) => {
+            target.parentNode.style.border = "1px solid black";
+        });
+    });
+    $inputs.forEach(item => {
+        item.addEventListener("focusout", ({ target }) => {
+            inputValidateCheck(target);
+            
+            target.parentNode.style.border = "#ddd";
+        });
+    });
+};
