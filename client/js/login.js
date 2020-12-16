efcaf2f27fa056a864a209171cba7b92a0b42dcd
Linkuad <linkuad@gmail.com>
2020-12-16 00:38:08 +0000

    Refactor: remove unnecessary code

--- client/js/login.js
+++ client/js/login.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-=======
-window.onload =_=>{
-    inputs.email.addEventListener('input', () => showErase(inputs.email))
-    inputs.password.addEventListener('input', () => showErase(inputs.password))
-    
-    inputs.email.addEventListener('focusout', () => inputChk(inputs.email))
-    inputs.password.addEventListener('focusout', () => inputChk(inputs.password))
-
-    inputs.email.addEventListener('focus', () => focus(inputs.email))
-    inputs.password.addEventListener('focus', () => focus(inputs.password))
-}
-
->>>>>>> 0ff475c4065ff241b7854cbea75919149aca9f75
 const inputs = {
     email : document.querySelector('input[name=email]'),
     password : document.querySelector('input[name=password]'),
@@ -41,29 +27,26 @@ const inputChk =ele=>{
     let err = ele.parentNode.children[2];
     err.innerHTML = '';
     if(ele == inputs.email){
-        var reg = /^[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[@]{1}[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[.]{1}[A-Za-z]{1,5}$/;;  
+        var reg = /^[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[@]{1}[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[.]{1}[A-Za-z]{1,5}$/;
         if(!reg.test(ele.value)){
             err.innerHTML = '이메일을 잘못 입력했습니다.'
         }
     }else if(ele == inputs.password){
-        var reg = /(?=.*[a-zA-Z]+)(?=.*[0-9]+)(?=.*[`~!@@#$%^&*|₩₩₩'₩";:₩/?]+).{8,20}/
+        var reg = /(?=.*[a-zA-Z]+)(?=.*[0-9]+)(?=.*[`~!@@#$%^&*|₩₩₩'₩";:₩/?]+).{8,20}/;
         if(!reg.test(ele.value)){
-            err.innerHTML = '비밀번호는 대문자, 특수문자를 포함하여 8자 이상 입력해야 합니다.'
+            err.innerHTML = '비밀번호는 대문자, 특수문자를 포함하여 8자 이상 입력해야 합니다.';
         }
     }
     ele.parentNode.style.border = '1px solid #ddd';
 }
-<<<<<<< HEAD
 
-window.onload =_=>{
-    inputs.email.addEventListener('input', () => showErase(inputs.email))
-    inputs.password.addEventListener('input', () => showErase(inputs.password))
-    
-    inputs.email.addEventListener('focusout', () => inputChk(inputs.email))
-    inputs.password.addEventListener('focusout', () => inputChk(inputs.password))
+window.onload = () =>{
+    inputs.email.addEventListener('input', () => showErase(inputs.email));
+    inputs.password.addEventListener('input', () => showErase(inputs.password));
 
-    inputs.email.addEventListener('focus', () => focus(inputs.email))
-    inputs.password.addEventListener('focus', () => focus(inputs.password))
+    inputs.email.addEventListener('focus', () => focus(inputs.email));
+    inputs.password.addEventListener('focus', () => focus(inputs.password));
+	
+    inputs.email.addEventListener('focusout', () => inputChk(inputs.email));
+    inputs.password.addEventListener('focusout', () => inputChk(inputs.password));
 }
-=======
->>>>>>> 0ff475c4065ff241b7854cbea75919149aca9f75
