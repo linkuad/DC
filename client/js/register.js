ce10ae11c20c8f730706ba8644424f3d7073ea37
wonsunil <seonil6352@gmail.com>
2020-12-17 12:19:56 +0900

    Refactor: remove focus in input

--- client/js/register.js
+++ client/js/register.js
@@ -1,43 +1,35 @@
 const inputValidateCheck = target => {
 	const $err = target.parentNode.children[2];
     $err.innerHTML = "";
-	
-	if(target.name === "email") {
+
+    if(target.name === "email") {
         const reg = /^[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[@]{1}[-A-Za-z0-9_]+[-A-Za-z0-9_.]*[.]{1}[A-Za-z]{1,5}$/;
         
         if (!reg.test(target.value)) {
-			target.focus();
-				
-        	return $err.innerHTML = "이메일을 잘못 입력했습니다.";
+            return $err.innerHTML = "이메일을 잘못 입력했습니다.";
         };
     };
     if(target.name === "nickname") {
         const reg = /^[가-힣|a-z|A-Z|0-9|\*]{2,12}$/;
-				
+
         if(!reg.test(target.value)) {
-			target.focus();
-					
             return $err.innerHTML = "닉네임을 잘못 입력했습니다.";
         };
     };
-	if(target.name === "password") {
+    if(target.name === "password") {
         const reg = /(?=.*[a-zA-Z]+)(?=.*[0-9]+).{8,20}/;
-				
+
         if (!reg.test(target.value)) {
-			target.focus();
-					
-		    return $err.innerHTML = "비밀번호는 대문자 포함하여 8자 이상 입력해야 합니다.";
+            return $err.innerHTML = "비밀번호는 대문자 포함하여 8자 이상 입력해야 합니다.";
         };
     };
-	if(target.name === "password_ck") {
-		if(document.forms[0].password.value !== target.value) {
-			target.focus();
-					
+    if(target.name === "password_ck") {
+        if(document.forms[0].password.value !== target.value) {
             return $err.innerHTML = "비밀번호를 일치하지 않습니다.";
         };
-	};
-	
-	return true;
+    };
+
+    return true;
 };
 
 window.onload = _ => {
@@ -45,8 +37,8 @@ window.onload = _ => {
 	
     document.forms[0].addEventListener("submit", event => {
         event.preventDefault();
-		
-		if(inputValidateCheck) event.target.submit();
+
+        if(inputValidateCheck) event.target.submit();
     });
     $inputs.forEach(item => {
         item.addEventListener("focus", ({ target }) => {
@@ -56,8 +48,8 @@ window.onload = _ => {
     $inputs.forEach(item => {
         item.addEventListener("focusout", ({ target }) => {
             inputValidateCheck(target);
-			
-			target.parentNode.style.border = "#ddd";
+
+            target.parentNode.style.border = "#ddd";
         });
     });
 };
