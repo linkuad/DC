4206878eb3bf04608fd41e84d47fb84c6268fe1b
jiseong-choi <jiseong1951@kakako.com>
2021-06-02 01:58:36 +0900

    isValidLoginForm

--- routes/users.js
+++ routes/users.js
@@ -10,11 +10,44 @@ const { User } = require("../models/User");
 router.post("/register", (req, res) => {
 
     const user = new User(req.body);
+    const {phone, email, password, name} = user;
+    if(phone && email && password && name) {
+        user.save((err, doc) => {
+            if (err) return res.json({ success: false, err });
+            return res.status(200).json({
+                success: true
+            });
+        });
+    } else {
+        return res.json({
+            success : false,
+            message : '정보를 입력해야 로그인을 할 수 있습니다.'
+        })
+    }
+});
 
-    user.save((err, doc) => {
-        if (err) return res.json({ success: false, err });
-        return res.status(200).json({
-            success: true
+
+router.post('/login', (req, res) => {// login function 
+	User.findOne({email: req.body.email}, (err, user) => {
+		if(!user) return res.json({
+			loginSuccess: false,
+			message: '로그인에 실패했습니다. 이메일을 찾을 수 없습니다.'
+		});
+		
+		user.comparePassword(req.body.password, (err, isMatch) => {
+			if(!isMatch) return res.json({ loginSuccess: false, message: "Wrong password" })
+			
+			user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+				
+                res.cookie("w_authExp", user.tokenExp);
+                res
+					.cookie("w_auth", user.token)
+                    .status(200)
+                    .json({
+                        loginSuccess: true, userId: user._id
+                    });
+            });
         });
     });
 });
--- server.js
+++ server.js
@@ -28,46 +28,11 @@ mongoose.connect(config.mongoURI, {//connect to MongoDB
 	console.log (`DB Connection Error: ${err.message}`);
 });
 
-app.use('/api/users', require('./routes/users'));
-
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname + "/client/index.html"));
 });
 
-// app.post('/users/register', (req,res) => {// register function
-// 	const user = new User(req.body);
-	
-// 	user.save((err,doc) => {
-// 		if(err) return res.json({success: false, err});
-		
-// 		return res.status(200).json({success: true, doc})//save실패하면 에러를 퉤 성공하면 정보를 퉤
-// 	});
-// });
-
-app.post('/users/login', (req, res) => {// login function 
-	User.findOne({email: req.body.email}, (err, user) => {
-		if(!user) return res.json({
-			loginSuccess: false,
-			message: '로그인에 실패했습니다. 이메일을 찾을 수 없습니다.'
-		});
-		
-		user.comparePassword(req.body.password, (err, isMatch) => {
-			if(!isMatch) return res.json({ loginSuccess: false, message: "Wrong password" })
-			
-			user.generateToken((err, user) => {
-                if (err) return res.status(400).send(err);
-				
-                res.cookie("w_authExp", user.tokenExp);
-                res
-					.cookie("w_auth", user.token)
-                    .status(200)
-                    .json({
-                        loginSuccess: true, userId: user._id
-                    });
-            });
-        });
-    });
-});
+app.use('/api/users', require('./routes/users'));
 
 app.listen(port, _ => {
 	console.log("express server open, port : " + port);
